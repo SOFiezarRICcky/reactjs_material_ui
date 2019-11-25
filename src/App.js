a0a4c23af0ab476e92f6ca578513841fa6a4cd0a
unknown <sofiezar195@gmail.com>
2019-11-25 13:13:25 +0700

    React JS Material UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      nama: ''
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let nama = this.state.name;
+    let val = event.target.value;
+    this.setState({
+      [nama]: val
+    });
+  }
+
+  handleNama = (event) => {
+    let nama = event.target.name;
+    let val = event.target.value;
+    this.setState({
+      [nama]: val
+    });
+  }
+
+  handleHilang = (event) => {
+    let nama = event.target.name;
+    let val = event.target.value;
+    this.setState({
+      nama: ""
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleNama}>
+          <Box component="span" display="block">
+            <TextField
+              id="standard-with-placeholder"
+              label="Nama Lengkap"
+              margin="normal"
+              onChange={this.handleNama}
+              name="nama"
+            >
+            </TextField>
+          </Box>
+          <Box component="span" display="block">
+            <Button variant="contained" color="primary" type="reset" onClick={this.handleHilang}>
+              Ganti Nama
+            </Button>
+          </Box>
+          <h1>HI. {this.state.nama}</h1>
+        </form>
+
+      </div>
+    );
+  }
 }
 
 export default App;
